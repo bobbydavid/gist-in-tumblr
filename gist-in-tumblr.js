a56d2e9a8dc6e318dad822f46311bb7a0f3e2f2c
Robert Martin <rdm2128@columbia.edu>
2012-04-29 20:18:09 -0400

    add boilerplate to embedding
    Open source! Spread the word :-)

--- gist-in-tumblr.js
+++ gist-in-tumblr.js
@@ -5,12 +5,18 @@
  *
  */
 $(document).ready(function(){
+  var bp='<div class="gist-in-tumblr" style="font-size:10px;text-align:right">'
+        +  'Embedded with '
+        +  '<a href="https://github.com/bobbydavid/gist-in-tumblr">'
+        +    'gist-in-tumblr'
+        +  '</a>.'
+        +'</div>';
   $("p.gist a:only-child").replaceWith(
     function(){
-    return '<div class="gist">'+this.href+'</div>';
+    return '<div class="gist"><div>'+this.href+'</div>'+bp+'</div>';
     }
   );
-  $("p.gist div.gist").each(function(){
+  $("p.gist div.gist div:first").each(function(){
     writeCapture.html(this,
       '<scr'+'ipt src="'+$(this).html()+'.js"></scr'+'ipt>'
     );
